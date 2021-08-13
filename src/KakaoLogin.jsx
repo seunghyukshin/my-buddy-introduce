949c968db476c600f07fe1c7b1eeb51de65d21a3
seunghyukshin <auseunghyuk@gmail.com>
2021-08-13 15:45:12 +0900

    #3 add a kakao login component

---
+++ src/KakaoLogin.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import KaKaoLogin from "react-kakao-login";
+
+import { KAKAO_API_KEY } from "./config.js";
+
+const KakaoLogin = function () {
+  const onKakaoSucess = (res) => {
+    console.log(res);
+  };
+  return (
+    <KaKaoLogin
+      token={KAKAO_API_KEY}
+      onSuccess={onKakaoSucess}
+      onFail={console.error}
+      onLogout={console.info}
+    />
+  );
+};
+
+export default KakaoLogin;
